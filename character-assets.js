71699bca96ab6df2388d740daaadbbcdd7533b36
bosonization <bosonization@gmail.com>
2026-04-29 17:58:40 +0900

    Add files via upload

--- character-assets.js
+++ character-assets.js
@@ -1,8 +1,5 @@
 window.characterAssets = {
   hero: 'assets/paka-ne-main.png',
   head: 'assets/paka-ne-head.png',
-  guide: 'assets/paka-ne-main.png',
-  pair: 'assets/paka-ne-main.png',
-  result: 'assets/paka-ne-main.png',
-  deep: 'assets/paka-ne-main.png'
+  ogp: 'ogp-paka-night-luxury-v2.jpg'
 };
